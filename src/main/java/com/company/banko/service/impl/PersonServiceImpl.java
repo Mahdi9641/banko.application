7880947e82094a14b5aad0653051c9bef8b9a940
HELP doc <mahdikhoddam2@gmail.com>
2022-03-08 17:20:58 +0330

    UPDATE

--- src/main/java/com/company/banko/service/impl/PersonServiceImpl.java
+++ src/main/java/com/company/banko/service/impl/PersonServiceImpl.java
@@ -2,6 +2,7 @@ package com.company.banko.service.impl;
 
 import com.company.banko.CustomAnnotation.CustomLog;
 import com.company.banko.domain.Person;
+import com.company.banko.model.CreatePersonRequest;
 import com.company.banko.repository.PersonRepository;
 import com.company.banko.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +33,8 @@ public class PersonServiceImpl implements PersonService {
 
     @Override
     @CustomLog
-    public Person insert(Person person) {
+    public Person insert(CreatePersonRequest createPersonRequest) {
+        Person person = new Person();
         person.setFirstName(person.getFirstName());
         person.setLastName(person.getLastName());
         person.setAge(person.getAge());

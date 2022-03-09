5358e53b4790696353d3e7a52c9d9e54aba0c297
HELP doc <mahdikhoddam2@gmail.com>
2022-03-09 13:49:06 +0330

    UPDATE

--- src/main/java/com/company/banko/controller/TransactionController.java
+++ src/main/java/com/company/banko/controller/TransactionController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 
 @RestController
-@RequestMapping("/banko/Transaction-Request")
+@RequestMapping("/banko/Transaction")
 public class TransactionController {
 
     @Autowired

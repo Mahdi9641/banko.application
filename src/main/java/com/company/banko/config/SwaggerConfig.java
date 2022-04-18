a32876fcf557fb3bd53cb696e353f37210a9f780
HELP doc <mahdikhoddam2@gmail.com>
2022-04-18 09:08:37 +0430

    UPDATE

--- src/main/java/com/company/banko/config/SwaggerConfig.java
+++ src/main/java/com/company/banko/config/SwaggerConfig.java
@@ -1,22 +1,12 @@
 package com.company.banko.config;
 
-
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.lang.reflect.Field;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Configuration
 @EnableSwagger2

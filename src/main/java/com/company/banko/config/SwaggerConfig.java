274d3edbd99ee2e80dff083f5ac56a39b9712a4b
Mahdi Khodam <mahdikhoddam2@gmail.com>
2022-06-26 14:12:39 +0430

    UPDATE

--- src/main/java/com/company/banko/config/SwaggerConfig.java
+++ src/main/java/com/company/banko/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.company.banko.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,6 +21,12 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
 
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("BANKING APPLICATION REST API")
+                .description("API for Banking Application.")
+                .version("1.0.0")
+                .build();
     }
 }
